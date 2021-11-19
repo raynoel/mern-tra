bec0e2eba8570c626a82fd278c6c5a0dc48485ef
nic <xfri3nd@gmail.com>
2021-11-19 05:21:49 -0500

    0702 Backend. Implémenté la route /api/user/login

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,21 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config();                                              // Active dotenv
-connectDB()                                                   // fct qui nous connecte à la DB avec mongoose
 
+connectDB()                                                   // fct qui nous connecte à la DB mongoose
 const app = express();                                        // initialise express
 
+// Middleware
+app.use(express.json())                                       // accepte JSON dans le body
 
 //--------------
 // Routes
 app.get('/', (req, res) => { res.send('API is running...') })
-app.use('/api/products', productRoutes)                     // Redirige '/api/products/' & '/api/products/:id' à "/routes/productRoutes.js"
+app.use('/api/products', productRoutes)                       // Redirige 'api/products/' et 'api/products/:id' à "/routes/productRoutes.js"
+app.use('/api/users', userRoutes)
 
 // Keep this as a last routes
 app.use(notFound)                                           // middleware gestion des 404 
