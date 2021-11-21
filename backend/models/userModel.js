c0ce2817c9658d228f4ac36835975d0e3fcc490b
nic <xfri3nd@gmail.com>
2021-11-21 09:02:59 -0500

    0707 Backend User registration

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,9 +10,21 @@ const userSchema = mongoose.Schema({
   { timestamps: true }
 )
 
+// matchPassword() vérifie si le pw match celui encodé dans la DB
 userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
+
+
+// Middleware de mongoose, appliqué lors de l'exécution de User.create() pour encoder et saler le pw
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 
 // Définit les champs nécessaires pour ajouter 'une rangée' à la DB-User
 const User = mongoose.model('User', userSchema)
