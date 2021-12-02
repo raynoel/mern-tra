55d766f0f3a6cb55f2006ba2d6c5848585388bf9
nic <xfri3nd@gmail.com>
2021-12-02 11:12:05 -0500

    1301 Backend - API pour ajouter une 'review'

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import Product from '../models/productModel.js'
-import { getProducts, getProductById, deleteProduct, createProduct, updateProduct } from '../controllers/productController.js'
+import { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
@@ -13,6 +13,7 @@ router.route('/:id')
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 
 
 export default router
